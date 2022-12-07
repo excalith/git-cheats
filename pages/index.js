8e352a2d9394fc9ee4eb52b5c463acb01d821b97
Can Cellek <excalith@users.noreply.github.com>
2022-12-08 01:10:38 +0300

    Add pre-commit hooks and lints (#26)
    * Add initial style and husky checks
    * Improve lint configs
    * Add install hook for husky
    * Fix format with prettier

--- pages/index.js
+++ pages/index.js
@@ -9,56 +9,62 @@ import Loader from "../components/Loader"
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
-export default function Home() {   
-	//Set up SWR to run the fetcher function when calling "/api/staticdata"
-	//There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
-	const { data, error } = useSWR("/api/staticdata", fetcher)
-	const [ search, setSearch ] = useState("")
-	const [ isAdvanced, setAdvanced ] = useState(false)
-	const { query: filterQuery } = useRouter()
+export default function Home() {
+    //Set up SWR to run the fetcher function when calling "/api/staticdata"
+    //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
+    const { data, error } = useSWR("/api/staticdata", fetcher)
+    const [search, setSearch] = useState("")
+    const [isAdvanced, setAdvanced] = useState(false)
+    const { query: filterQuery } = useRouter()
 
-	const handleSearch = (e) => {
-		setSearch(e.target.value)
-	}
+    const handleSearch = (e) => {
+        setSearch(e.target.value)
+    }
 
-	const handleAdvanced = () => { 
-		setAdvanced(!isAdvanced) 
-	} 
+    const handleAdvanced = () => {
+        setAdvanced(!isAdvanced)
+    }
 
-	useEffect(() => {
-		if (filterQuery.filter !== undefined) {
-			setSearch("filter=" + filterQuery.filter)
-		}
-	}, [filterQuery.filter])
+    useEffect(() => {
+        if (filterQuery.filter !== undefined) {
+            setSearch("filter=" + filterQuery.filter)
+        }
+    }, [filterQuery.filter])
 
-	//Handle the error state
-	if (error) return <div>Failed to load</div>
+    //Handle the error state
+    if (error) return <div>Failed to load</div>
 
-	//Handle the loading state
-	if (!data) return <Loader/>
+    //Handle the loading state
+    if (!data) return <Loader />
 
-	let commands = JSON.parse(data)
+    let commands = JSON.parse(data)
 
-	return (
-		<div className="container container-sm">
-			<SEO/>
-        	<Search handleSearch={handleSearch} handleAdvanced={handleAdvanced}/>
-          
-        	<main className="main">
-        		<div>
-        			{commands.map((card, index) => {
-        				{
-        					return (
-        						<Card key={index} data={card} query={search} isAdvanced={isAdvanced}/>
-        					)
-        				}
-        			})}
-        		</div>
+    return (
+        <div className="container container-sm">
+            <SEO />
+            <Search
+                handleSearch={handleSearch}
+                handleAdvanced={handleAdvanced}
+            />
 
-        		<Footer/>
-        	</main>
+            <main className="main">
+                <div>
+                    {commands.map((card, index) => {
+                        {
+                            return (
+                                <Card
+                                    key={index}
+                                    data={card}
+                                    query={search}
+                                    isAdvanced={isAdvanced}
+                                />
+                            )
+                        }
+                    })}
+                </div>
 
-
-		</div>
-	)
+                <Footer />
+            </main>
+        </div>
+    )
 }
