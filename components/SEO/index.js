c699c410328818fa90c1f5fa7bfe629516c5b89e
Can Cellek <excalith@users.noreply.github.com>
2022-12-08 01:02:17 +0300

    Fix format with prettier

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -1,26 +1,35 @@
 import Head from "next/head"
 
 const SEO = () => {
-	const title = "Git Cheats • Interactive Git Cheatsheet"
-	const description = "Interactive cheatsheet for git commands to ease the pain while transitioning from sluggish GUI clients to blazing-fast terminal commands"
-	// const socialImage = "https://cancellek.com/social-image.png"
+    const title = "Git Cheats • Interactive Git Cheatsheet"
+    const description =
+        "Interactive cheatsheet for git commands to ease the pain while transitioning from sluggish GUI clients to blazing-fast terminal commands"
+    // const socialImage = "https://cancellek.com/social-image.png"
 
-	return (
-		<Head>
-			<title>{title}</title>
-			<meta name="description" content={description} />
-			{/* {socialImage && <meta property="image" content={socialImage} />} */}
-			<meta property="og:title" content={title} key="title" />
-			<meta property="og:description" content={description} key="description"/>
-			{/* {socialImage && <meta property="og:image" content={socialImage} key="image" />} */}
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            {/* {socialImage && <meta property="image" content={socialImage} />} */}
+            <meta property="og:title" content={title} key="title" />
+            <meta
+                property="og:description"
+                content={description}
+                key="description"
+            />
+            {/* {socialImage && <meta property="og:image" content={socialImage} key="image" />} */}
 
-			<meta name="twitter:title" content={title} key="title"/>
-			<meta name="twitter:description" content={description} key="description"/>
-			{/* {socialImage && <meta name="twitter:image" content={socialImage} key="image"/>} */}
+            <meta name="twitter:title" content={title} key="title" />
+            <meta
+                name="twitter:description"
+                content={description}
+                key="description"
+            />
+            {/* {socialImage && <meta name="twitter:image" content={socialImage} key="image"/>} */}
 
-			<link rel="icon" href="/favicon.ico" />
-		</Head>
-	)
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    )
 }
 
 export default SEO
--- components/Search/index.js
+++ components/Search/index.js
@@ -1,30 +1,43 @@
 import React, { useRef, useEffect } from "react"
 
-const Search = ({ handleSearch, handleAdvanced,...rest }) => {
-	const inputElement = useRef(null)
+const Search = ({ handleSearch, handleAdvanced, ...rest }) => {
+    const inputElement = useRef(null)
 
-	useEffect(() => {
-		if (inputElement.current) {
-			inputElement.current.focus()
-		}
-	}, [])
+    useEffect(() => {
+        if (inputElement.current) {
+            inputElement.current.focus()
+        }
+    }, [])
 
-	return (
-		<div className="sticky-top search-row row g-2">
-			<input
-				className='col form-control search-bar'
-				type="text"
-				onChange={handleSearch}
-				placeholder="Search"
-				autoFocus
-				ref={inputElement}
-			/>
-			<div className="col-sm-2 btn-group search-advanced" role="group">
-				<input type="checkbox" className="btn-check" id="advanced-check" autoComplete="off" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Tooltip on left" onChange={handleAdvanced}/>
-				<label className="btn btn-outline-warning" htmlFor="advanced-check">Advanced</label>
-			</div>
-		</div>
-	)
+    return (
+        <div className="sticky-top search-row row g-2">
+            <input
+                className="col form-control search-bar"
+                type="text"
+                onChange={handleSearch}
+                placeholder="Search"
+                autoFocus
+                ref={inputElement}
+            />
+            <div className="col-sm-2 btn-group search-advanced" role="group">
+                <input
+                    type="checkbox"
+                    className="btn-check"
+                    id="advanced-check"
+                    autoComplete="off"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="left"
+                    data-bs-title="Tooltip on left"
+                    onChange={handleAdvanced}
+                />
+                <label
+                    className="btn btn-outline-warning"
+                    htmlFor="advanced-check">
+                    Advanced
+                </label>
+            </div>
+        </div>
+    )
 }
 
 export default Search
