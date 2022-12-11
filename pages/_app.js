abde9291ab05f13f1fbdb9b0916194d118d3be03
Can Cellek <excalith@users.noreply.github.com>
2022-12-11 16:57:47 +0300

    Support Multiple Languages Through Language API  (#27)
    * Add language support to language api
    * Improve Get Method check

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,6 @@ import "../styles/code.css"
 import "../styles/footer.css"
 
 function MyApp({ Component, pageProps }) {
-	console.log(
-		"Hey there!\nInstead of peeking into the code,\nyou can find the whole project on\nhttps://github.com/excalith/git-cheats"
-	)
 	return <Component {...pageProps} />
 }
 
---
+++ pages/api/language.js
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// https://vercel.com/guides/loading-static-file-nextjs-api-route
+import path from "path"
+import { promises as fs } from "fs"
+import { DEFAULT_LANGUAGE, AVAILABLE_LANGUAGES } from "../../gitcheats.config"
+
+export default async function handler(req, res) {
+	// Only GET method is allowed
+	if (req.method != "GET") {
+		res.status(405).end()
+		return
+	}
+
+	// Get requested language
+	let queryLang = req.query.lang
+
+	// Set default language
+	let language = DEFAULT_LANGUAGE
+
+	// Check query language if valid. Else, continue with default language
+	if (queryLang != "undefined" && AVAILABLE_LANGUAGES.includes(queryLang)) {
+		language = queryLang
+	}
+
+	// Find the absolute path of the json directory
+	const dataDirectory = path.join(process.cwd(), "data/language")
+
+	// Read the json data file data.json
+	const fileContents = await fs.readFile(
+		dataDirectory + "/" + language + ".json",
+		"utf8"
+	)
+
+	// Return the content of the data file in json format
+	res.status(200).json(fileContents)
+}
