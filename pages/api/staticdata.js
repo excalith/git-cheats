c6ac26e7dab79389b56a0902c2b2d3a0420e640a
Can Cellek <excalith@users.noreply.github.com>
2022-12-11 16:16:37 +0300

    Improve Eslint and Prettier integration, Add jsconfig.json LSP

--- pages/api/staticdata.js
+++ pages/api/staticdata.js
@@ -5,10 +5,10 @@ import path from "path"
 import { promises as fs } from "fs"
 
 export default async function handler(req, res) {
-    //Find the absolute path of the json directory
-    const jsonDirectory = path.join(process.cwd(), "data")
-    //Read the json data file data.json
-    const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8")
-    //Return the content of the data file in json format
-    res.status(200).json(fileContents)
+	//Find the absolute path of the json directory
+	const jsonDirectory = path.join(process.cwd(), "data")
+	//Read the json data file data.json
+	const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8")
+	//Return the content of the data file in json format
+	res.status(200).json(fileContents)
 }
