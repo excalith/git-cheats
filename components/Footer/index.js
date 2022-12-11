2cd197ba11c420282af3e73388656a09556e6004
Can Cellek <excalith@users.noreply.github.com>
2022-12-11 16:58:34 +0300

    Improve footer color, change link to repository

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,7 +6,12 @@ const Footer = () => {
 		<div className="footer wh-100">
 			<p>
 				Made with <span>&hearts;</span> by{" "}
-				<Link href="https://github.com/excalith">excalith</Link>
+				<Link
+					target="_blank"
+					rel="noopener noreferrer nofollow"
+					href="https://github.com/excalith/git-cheats">
+					excalith
+				</Link>
 			</p>
 		</div>
 	)
