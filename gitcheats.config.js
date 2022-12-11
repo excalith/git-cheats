ac1468cbd0c3198bd7cc015d9ca39128409e905e
Can Cellek <excalith@users.noreply.github.com>
2022-12-11 16:23:20 +0300

    Add test language

--- gitcheats.config.js
+++ gitcheats.config.js
@@ -1,2 +1,2 @@
 export const DEFAULT_LANGUAGE = "en"
-export const AVAILABLE_LANGUAGES = ["en"]
+export const AVAILABLE_LANGUAGES = ["en", "es"]
--- pages/api/language.js
+++ pages/api/language.js
@@ -2,19 +2,20 @@
 // https://vercel.com/guides/loading-static-file-nextjs-api-route
 import path from "path"
 import { promises as fs } from "fs"
-import {DEFAULT_LANGUAGE, AVAILABLE_LANGUAGES} from "../../gitcheats.config"
+import { DEFAULT_LANGUAGE, AVAILABLE_LANGUAGES } from "../../gitcheats.config"
 
 export default async function handler(req, res) {
 	// Only GET Method allowed
-	if (req.method != "GET")
-		res.status(405).end()
-	
+	if (req.method != "GET") res.status(405).end()
+
 	// Set default language
 	let language = DEFAULT_LANGUAGE
 
 	// Check query for a valid language
-	if (req.query.lang != "undefined" && AVAILABLE_LANGUAGES.includes(req.query.lang))
-	{
+	if (
+		req.query.lang != "undefined" &&
+		AVAILABLE_LANGUAGES.includes(req.query.lang)
+	) {
 		language = req.query.lang
 	}
 
@@ -22,7 +23,10 @@ export default async function handler(req, res) {
 	const dataDirectory = path.join(process.cwd(), "data/language")
 
 	//Read the json data file data.json
-	const fileContents = await fs.readFile(dataDirectory + "/" + language + ".json", "utf8")
+	const fileContents = await fs.readFile(
+		dataDirectory + "/" + language + ".json",
+		"utf8"
+	)
 
 	//Return the content of the data file in json format
 	res.status(200).json(fileContents)
