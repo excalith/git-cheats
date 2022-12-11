51df5b40f2462d8ac2cdfa3a3eee3df1f9f1250c
Can Cellek <excalith@users.noreply.github.com>
2022-12-11 16:52:55 +0300

    Improve Get Method check

--- pages/api/language.js
+++ pages/api/language.js
@@ -5,29 +5,32 @@ import { promises as fs } from "fs"
 import { DEFAULT_LANGUAGE, AVAILABLE_LANGUAGES } from "../../gitcheats.config"
 
 export default async function handler(req, res) {
-	// Only GET Method allowed
-	if (req.method != "GET") res.status(405).end()
+	// Only GET method is allowed
+	if (req.method != "GET") {
+		res.status(405).end()
+		return
+	}
+
+	// Get requested language
+	let queryLang = req.query.lang
 
 	// Set default language
 	let language = DEFAULT_LANGUAGE
 
-	// Check query for a valid language
-	if (
-		req.query.lang != "undefined" &&
-		AVAILABLE_LANGUAGES.includes(req.query.lang)
-	) {
-		language = req.query.lang
+	// Check query language if valid. Else, continue with default language
+	if (queryLang != "undefined" && AVAILABLE_LANGUAGES.includes(queryLang)) {
+		language = queryLang
 	}
 
-	//Find the absolute path of the json directory
+	// Find the absolute path of the json directory
 	const dataDirectory = path.join(process.cwd(), "data/language")
 
-	//Read the json data file data.json
+	// Read the json data file data.json
 	const fileContents = await fs.readFile(
 		dataDirectory + "/" + language + ".json",
 		"utf8"
 	)
 
-	//Return the content of the data file in json format
+	// Return the content of the data file in json format
 	res.status(200).json(fileContents)
 }
