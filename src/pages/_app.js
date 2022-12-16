c1c8062f6fd196889477b592a9214bb5855ed873
Can Cellek <excalith@users.noreply.github.com>
2022-12-16 12:53:23 +0300

    Update folder structure (#28)

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.css"
+import "../../styles/globals.css"
+import "../../styles/home.css"
+import "../../styles/search.css"
+import "../../styles/card.css"
+import "../../styles/code.css"
+import "../../styles/footer.css"
+
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />
+}
+
+export default MyApp
